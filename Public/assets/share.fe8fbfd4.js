5f7484e15f45d9ca7623a3c1c850f04a17235683
WangNingkai <1655586865@qq.com>
2021-02-19 16:46:13 +0800

    [#] pack frontend

--- Public/assets/share.fe8fbfd4.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./index.8f74753c.js";const s=s=>a.get("/api/share",s),e=s=>a.post("/api/share",s),p=(s,e)=>a.delete("/api/share/"+s,e),r=(s,e)=>a.get("/api/share/"+s,e);export{r as a,e as c,p as d,s};
--- Public/assets/teambition.6a4fdb6c.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.8f74753c.js";const s=s=>o.post("/api/nodes",s),a=s=>o.post("/api/node",s);export{a,s as f};
